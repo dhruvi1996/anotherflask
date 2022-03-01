545b5194bf3de0dc9d3391ef926403cea2119a72
dhruvi1996 <ddm8@njit.edu>
2022-03-01 15:30:15 -0500

    change in the simples pages about foodies page and about

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -16,7 +16,7 @@ def test_request_about(client):
     """This makes the index page"""
     response = client.get("/about")
     assert response.status_code == 200
-    assert b"about" in response.data
+    assert b"/about" in response.data
 
 def test_request_page_not_found(client):
     """This makes the index page"""

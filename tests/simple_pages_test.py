cecc538a2d22cfea4d29a1f29d79cf15abe1ecbe
dhruvi1996 <ddm8@njit.edu>
2022-03-01 15:41:17 -0500

    change in the simples pages about foodies page and about

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,7 +4,7 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/about">About</a>' in response.data
+    assert b'<a class="nav-link" aria-current="page" href="/about">About</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
@@ -16,7 +16,7 @@ def test_request_about(client):
     """This makes the index page"""
     response = client.get("/about")
     assert response.status_code == 200
-    assert b"/about" in response.data
+    assert b"About" in response.data
 
 def test_request_page_not_found(client):
     """This makes the index page"""
